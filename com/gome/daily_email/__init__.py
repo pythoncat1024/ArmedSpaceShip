a65994d96591bc1d4dd61beeec19357d99693dc5
pythonCat <pythoncat.cheng@gometech.com.cn>
2018-02-02 15:37:15 +0800

    [feat]: 闭包的实际使用，为了无限定时任务

--- com/gome/daily_email/__init__.py
+++ com/gome/daily_email/__init__.py
@@ -10,3 +10,29 @@
                    2018/1/24:
 -------------------------------------------------
 """
+
+
+def timer(delay, func):
+    import threading
+    threading.Timer(delay, func).start()
+
+
+def showPower(delay):
+    """
+    好像只能通过这种所谓“闭包”的方式实现无限定时任务了
+    :param delay:
+    :return:
+    """
+
+    def show():
+        import time
+        import threading
+        print(threading.current_thread().getName(), time.time())
+        threading.Timer(delay, show).start()
+
+    show()
+
+
+if "__main__" == __name__:
+    showPower(3)
+    pass
